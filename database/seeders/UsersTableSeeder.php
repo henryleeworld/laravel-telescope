e76a8b4c615e3dbdaf71712e4df483de0beed32b
Henry Lee <henryleeworld@gmail.com>
2020-09-10 19:34:20 +0800

    feat:升級至 laravel 8

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace Database\Seeders;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,6 +14,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 50)->create();
+        User::factory()->times(50)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/user/{id}', 'UserController@show')->where('id', '[0-9]+');
+Route::get('/user/{id}', [UserController::class, 'show'])->where('id', '[0-9]+');
